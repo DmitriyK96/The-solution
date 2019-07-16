9b5820657bf27f8d1a2182da24bc2a28d8c4bef1
DmitriyK96 <52975317+DmitriyK96@users.noreply.github.com>
2019-07-16 22:01:18 +0300

    Add files via upload

---
+++ task/index.php
@@ -0,0 +1,28 @@
+﻿<?php
+function fibonacci($n)
+{
+	$nn = 0;
+	$n0 = 0;
+	$n1 = 1;
+
+	while ($nn <= $n ){
+		switch ($nn) {
+			case 0:
+				$r = $n0;
+				break;
+			case 1:
+				$r = $n1;
+				break;
+			default:	
+				$r = $n0 + $n1;
+				$n0 = $n1;
+				$n1 = $r;
+			}
+			echo 'Номер '.$nn.' Значение '.$r;
+			echo "<br>";
+			$nn++;
+	}
+}
+
+fibonacci(64);
+?>
